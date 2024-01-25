2ec0a2aa289135106352fdd2fc94139061a84504
anurag agarwal <anuragagarwal188@gmail.com>
2024-01-26 03:06:18 +0530

    fixed

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -15,7 +15,7 @@ const Avatar = ({ img, other }:
     return (
         <div className="relative">
             <div className="relative inline-block rounded-full overflow-hidden w-10 h-10">
-                <Image alt="Avatar" src={img || '/images/placeholder.jpg'} fill />
+              {!!img ? <Image alt="Avatar" src={img} fill /> : <Image alt="Avatar" src='/images/placeholder.jpg' fill />} 
             </div>
             {!other && <span
                 className="absolute block rounded-full bg-green-500 ring-2 ring-white top-0 right-0 h-2 w-2"
